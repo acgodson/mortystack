ea0d63fa71e9e242e0ce2a390bf7d5f8a1d7fa0e
kelvinpraises <kelvinpraises@gmail.com>
2023-11-18 10:55:40 +0100

    feat: add shop details

--- morty-frontend/src/components/CreateShop/index.tsx
+++ morty-frontend/src/components/CreateShop/index.tsx
@@ -1,111 +1,153 @@
-// components/HomePage.js
+import { useWeb3AuthProvider } from "@/contexts/Web3AuthContext";
+import { getShopsByOid } from "@/libs/shops";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Stack,
+  Text,
+  VStack,
+  useToast,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import CreateShopButton from "./CreateShopButton";
 
-import { Box, Center, Flex, HStack, Heading, SimpleGrid, Stack, Text } from '@chakra-ui/react';
-import CreateShopButton from './CreateShopButton';
+interface Shop {
+  description: string;
+  name: string;
+  oid: string;
+  products: string[];
+}
 
-//list out users created shops
-// const shops = [
-//     { id: 1, name: 'Shop 1' },
-//     { id: 2, name: 'Shop 2' },
-//     // Add more shops as needed
-// ];
+const HomePage: React.FC = () => {
+  const toast = useToast();
 
-const HomePage = () => {
-    return (
-        <Box pt={32} px={[3, 3, 3, 20]}>
-            {/* Top Banner */}
-            <Stack
-                direction={["column-reverse", "column-reverse", "column-reverse", "row"]}
-                mb={5}
-                w="100%"
-                justifyContent={["center", "center", "center", "space-around"]}
-                h={["fit-content", "fit-content", "fit-content", "300px"]}
-                spacing={[0, 0, 0, 8]}
-                bg="#132036"
-                mt={[12, 12, 12, 0]}
-                pb={[12, 12, 12, 18]}
-                pt={[0, 0, 0, 18]}
-                color="white">
+  const { organizations }: any = useWeb3AuthProvider();
+  const [shops, setShops] = useState<Shop[]>([]);
 
-                <Center >
-                    <Box
-                        pt={[0, 0, 0, 8]}
-                        h="100%"
-                        display={"flex"}
-                        flexDirection={"column"}
-                        justifyContent={"center"}
-                        alignItems={"center"}
-                        px={3}>
-                        <Box
-                            h="100%"
-                            fontSize={["md", "md", "md", "2xl"]}
-                        >
-                            <Box
-                                fontWeight={"semibold"}
-                                as="h1" >
-                                Keep the income flowing!
-                            </Box>
+  const [host, setHost] = useState("");
 
+  useEffect(() => {
+    let hostname = window.location.hostname;
+    const hostnameParts = (hostname + "").split(".");
+    let mainDomain = hostnameParts[hostnameParts.length - 1];
+    const port = window.location.port;
+    const newHost = port ? `.${mainDomain}:${port}` : `.${mainDomain}`;
+    setHost(newHost);
+  }, []);
 
-                            <br />
-                            <Text
-                                maxW="600px"
-                                fontSize={["sm", "sm", "sm", "md"]}>
-                                Host a web page for free with Morty and start accepting payments in form of Assets for your products.</Text>
-                        </Box>
-                    </Box>
-                </Center>
+  useEffect(() => {
+    // get shop by OID
+    const myCallback = (error: string, shops?: any[]) => {
+      if (error) {
+        toast({
+          status: "error",
+          description: error,
+        });
+      } else {
+        setShops(shops || []);
+      }
+    };
 
+    organizations && getShopsByOid(organizations[0]?.oid, myCallback);
+  }, [organizations && organizations[0]]);
 
-                <Box>
-                    <Box
-                        h="250px"
-                        as="img" src="/oShop.svg" />
-                </Box>
-            </Stack>
+  return (
+    <Box pt={32} px={[3, 3, 3, 20]}>
+      {/* Top Banner */}
+      <Stack
+        direction={[
+          "column-reverse",
+          "column-reverse",
+          "column-reverse",
+          "row",
+        ]}
+        mb={5}
+        w="100%"
+        justifyContent={["center", "center", "center", "space-around"]}
+        h={["fit-content", "fit-content", "fit-content", "300px"]}
+        spacing={[0, 0, 0, 8]}
+        bg="#132036"
+        mt={[12, 12, 12, 0]}
+        pb={[12, 12, 12, 18]}
+        pt={[0, 0, 0, 18]}
+        color="white"
+      >
+        <Center>
+          <Box
+            pt={[0, 0, 0, 8]}
+            h="100%"
+            display={"flex"}
+            flexDirection={"column"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            px={3}
+          >
+            <Box h="100%" fontSize={["md", "md", "md", "2xl"]}>
+              <Box fontWeight={"semibold"} as="h1">
+                Keep the income flowing!
+              </Box>
 
-            {/* Shop Grid */}
-            <SimpleGrid columns={[1, 2, 3]} spacing={4} p={4}>
+              <br />
+              <Text fontSize={["sm", "sm", "sm", "md"]}>
+                Host your website for free with Morty and start accepting
+                payments in form of Assets.
+              </Text>
+            </Box>
+          </Box>
+        </Center>
 
+        <Box>
+          <Box h="250px" as="img" src="/oShop.svg" />
+        </Box>
+      </Stack>
 
-                <Flex
-                    as="button"
-                    maxW="250px"
-                    h={["fit-content", "fit-content", "fit-content", "250"]}
-                    align="center"
-                    justify="center"
-                    bg="#182942"
-                    p={8}
-                    borderRadius="md"
-                >
+      {/* Shop Grid */}
+      <SimpleGrid columns={[1, 2, 3]} spacing={4} p={4}>
+        <Flex
+          as="button"
+          maxW="250px"
+          h={["fit-content", "fit-content", "fit-content", "250"]}
+          align="center"
+          justify="center"
+          bg="#182942"
+          p={8}
+          borderRadius="md"
+        >
+          <Box>
+            <CreateShopButton setShops={setShops} />
+          </Box>
+        </Flex>
 
-
-                    <Box>
-                        <CreateShopButton />
-                    </Box>
-
-                </Flex>
-
-                {/* <Box
-                    key={shop.id}
-                    bg="white"
-                    p={4}
-                    w="100%"
-                    maxW="5˝00px"
-                    borderRadius="md"
-                    boxShadow="md"
-                >
-                    <Heading as="h2" fontSize="xl" mb={2}>
-                        {shop.name}
-                    </Heading>
-                    {/* Add more details or links for each shop as needed */}
-                {/* <Text>Shop Description or Additional Information</Text>
-                </Box>  */}
-
-
-            </SimpleGrid >
-        </Box >
-    );
+        {shops.map(({ name, description }, i) => {
+          return (
+            <Flex
+              key={i}
+              as="a"
+              href={`http://${name}${host}`}
+              target="_blank"
+              maxW="250px"
+              h={["fit-content", "fit-content", "fit-content", "250"]}
+              bg="#182942"
+              alignItems={"center"}
+              justifyContent={"center"}
+              p={8}
+              borderRadius="md"
+            >
+              <VStack align="center">
+                <Heading as="h2" fontSize="xl" mb={2}>
+                  {name}
+                </Heading>
+                <Text textAlign="center">{description}</Text>
+              </VStack>
+            </Flex>
+          );
+        })}
+      </SimpleGrid>
+    </Box>
+  );
 };
 
 export default HomePage;
